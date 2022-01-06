286572254d3c1944c13eeb62423f788dbce60afb
Rukchad Wongprayoon <contact@biomooping.tk>
2022-01-06 21:51:02 +0700

    lez go

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import discord
 from discord.ext import commands
+import src.cogs.datas.slash_utils as slash
 import os
 import keep_alive
+import requests
 
-bot = commands.Bot(command_prefix='a!',intents=discord.Intents().all())
+bot = slash.Bot(command_prefix='a!',intents=discord.Intents().all())
 
 @bot.event
 async def on_ready():
@@ -13,9 +15,13 @@ async def on_ready():
     print('------')
 
 for cog in os.listdir('./src/cogs'):
-    if cog.endswith('.py'):
-        cog = f'src.cogs.{cog[:-3]}'
-        bot.load_extension(cog)
+	if cog.endswith('.py'):
+		bot.load_extension("src.cogs."+cog[:-3])
 
+try:
+    r = requests.head(url="https://discord.com/api/v1")
+except Exception:
+    print(f"Rate limit {round(int(r.headers['Retry-After']) / 60, 2)} minutes left")
+		
 keep_alive.keep_alive()
 bot.run(os.environ["TOKEN"])
--- src/cogs/slash_application_manager.py
+++ src/cogs/slash_application_manager.py
@@ -7,12 +7,15 @@ import string
 import time
 from datetime import datetime
 import asyncio
-import src.cogs.datas.slash_utils as commands
+import src.cogs.datas.slash_utils as slash
 
 class SlashApplicationManager(ApplicationCog):
 	
 	def __init__(self,bot):
+		print("SlashApplicationManager is loaded.")
+		super().__init__(bot)
 		self.bot = bot
+		self.admin = 911930082673696798
 		self.applicable = {
 			"staff":self.staff,
 			"support":self.support
@@ -43,16 +46,18 @@ class SlashApplicationManager(ApplicationCog):
 				911930501613359124
 			]
 		}
-	@commands.slash_command()
-	async def apply(self,ctx,apply_=None):
-		if apply_ is None:
+	@slash.slash_command()
+	@slash.describe(apply_="Application name")
+	async def apply(self,ctx:slash.Context,apply_:str):
+		"""
+		This command for apply the application.
+		"""
+		if apply_ not in list(self.applicable):
 			a = '\n'.join(list(self.applicable))
 			return await ctx.send(embed=discord.Embed(title="Application not found",description=f"You cannnot supply empty application name.\nAvailable applications:\n\n{a}", color=0x00e5ff))
-		if apply_ not in list(self.applicable):
-			return await ctx.send("Your application seem doesn't exist *yet*.")
 		await self.applicable[apply_](ctx)
 
-	async def staff(self,ctx):
+	async def staff(self,ctx:slash.Context):
 		await ctx.author.send(embed=discord.Embed(title="Staff applying process.",description="I have started this application process.", color=0x00ffd2))
 		questions = self.question["staff"]
 		await ctx.send("Check your direct messages!👀")
@@ -93,8 +98,19 @@ class SlashApplicationManager(ApplicationCog):
 		async with aiofiles.open("src/cogs/datas/applications.json","w") as fp:
 			await fp.write(json.dumps(db,indent=4))
 	
-	@commands.slash_command()
-	async def accept(self,ctx,app_id):
+	@slash.slash_command()
+	@slash.describe(app_id="Application ID",reason="Reason why you accept this application.")
+	async def accept(self,ctx:slash.Context,app_id:str,*,reason:str="No reason provided"):
+		"""
+		This command for accepting the application.
+		"""
+		roles = []
+		for role in ctx.author.roles:
+			roles.append(role.id)
+		if self.admin not in roles:
+			return await ctx.send("You don't have permission to do this!")
+		if app_id == "":
+			return await ctx.send("No application ID supplied")
 		for role in ctx.author.roles:
 			if role.id == 911930082673696798:
 				async with aiofiles.open("src/cogs/datas/applications.json") as fp:
@@ -103,21 +119,22 @@ class SlashApplicationManager(ApplicationCog):
 				if app_id not in list(db):
 					return await ctx.send("There's no guy with that ID? What do you mean?")
 		
-				await self.acceptance[db[app_id]["type"]](ctx,db,app_id)
+				await self.acceptance[db[app_id]["type"]](ctx,db,app_id,reason)
+				await ctx.send("Successfully applied application and done the process.")
 		
-	async def staff_accept(self,ctx,db,app_id):
+	async def staff_accept(self,ctx:slash.Context,db,app_id,reason):
 		user = ctx.guild.get_member(int(db[app_id]["name"].strip("<@>")))
 		for role in ctx.guild.roles:
 			for role_id in self.roles["staff"]:
 				if role_id == role.id:
 					print(f"Adding {role.name} which is {role.id}")
 					await user.add_roles(role)
-		await user.send(f"Congratulations! Your staff application is accepted! in {ctx.guild.name} by {ctx.author.mention}")
+		await user.send(embed=discord.Embed(title=f"Congratulations! Your staff application is accepted! in {ctx.guild.name} by {ctx.author.mention}",description=f"Congratulations! Your staff application is accepted! in {ctx.guild.name} by {ctx.author.mention}\nReason:\n{reason}"))
 		del db[app_id]
 		async with aiofiles.open("src/cogs/datas/applications.json","w") as fp:
 			await fp.write(json.dumps(db,indent=4))
 
-	async def support(self,ctx):
+	async def support(self,ctx:slash.Context):
 		await ctx.author.send(embed=discord.Embed(title="Support applying process.",description="what ever sentence u want"))
 		questions = self.question["support"]
 		await ctx.send("Check your direct messages!👀")
@@ -157,24 +174,35 @@ class SlashApplicationManager(ApplicationCog):
 		async with aiofiles.open("src/cogs/datas/applications.json","w") as fp:
 			await fp.write(json.dumps(db,indent=4))
 	
-	async def support_accept(self,ctx,db,app_id):
+	async def support_accept(self,ctx:slash.Context,db,app_id,reason):
 		user = ctx.guild.get_member(int(db[app_id]["name"].strip("<@>")))
 		for role in ctx.guild.roles:
 			for role_id in self.roles["support"]:
 				if role_id == role.id:
 					print(f"Adding {role.name} which is {role.id}")
 					await user.add_roles(role)
-		await user.send(f"Congratulations! Your support application is accepted! in {ctx.guild.name} by {ctx.author.mention}")
+		await user.send(embed=discord.Embed(title=f"Congratulations! Your support application is accepted! in {ctx.guild.name} by {ctx.author.mention}",description=f"Congratulations! Your support application is accepted! in {ctx.guild.name} by {ctx.author.mention}\nReason:\n{reason}"))
 		del db[app_id]
 		async with aiofiles.open("src/cogs/datas/applications.json","w") as fp:
 			await fp.write(json.dumps(db,indent=4))
-	@commands.slash_command()
-	async def applications(self,ctx):
+	@slash.slash_command()
+	async def applications(self,ctx:slash.Context):
+		"""
+		This command for listing application.
+		"""
 		a = '\n'.join(list(self.applicable))
 		await ctx.send(embed=discord.Embed(title='Available Applications',description=f"{a}"))
 					   
-	@commands.slash_command()
-	async def lists(self,ctx):
+	@slash.slash_command()
+	async def lists(self,ctx:slash.Context):
+		"""
+		This command for list all pending application.
+		"""
+		roles = []
+		for role in ctx.author.roles:
+			roles.append(role.id)
+		if self.admin not in roles:
+			return await ctx.send("You don't have permission to do this!")
 		for role in ctx.author.roles:
 			if role.id == 911930082673696798:
 				embeds = []
@@ -193,8 +221,19 @@ class SlashApplicationManager(ApplicationCog):
 					await ctx.send(embed=embed)
 					await asyncio.sleep(0.3)
 					
-	@commands.slash_command()
-	async def decline(self,ctx,app_id,*,reason="No reason provided"):
+	@slash.slash_command()
+	@slash.describe(app_id="Application ID",reason="A reason why you decline this application.")
+	async def decline(self,ctx:slash.Context,app_id:str,*,reason:str="No reason provided"):
+		"""
+		This command for declining application.
+		"""
+		roles = []
+		for role in ctx.author.roles:
+			roles.append(role.id)
+		if self.admin not in roles:
+			return await ctx.send("You don't have permission to do this!")
+		if app_id == "":
+			return await ctx.send("No application ID supplied")
 		async with aiofiles.open("src/cogs/datas/applications.json") as fp:
 			db = json.loads(await fp.read())
 		if app_id not in list(db):
@@ -207,7 +246,7 @@ class SlashApplicationManager(ApplicationCog):
 		async with aiofiles.open("src/cogs/datas/applications.json","w") as fp:
 			await fp.write(json.dumps(db,indent=4))
 		await user.send(embed=discord.Embed(title=f"Your {type} application is declined!",description=f"Reason:\n{reason}"))
-		await a.edit(embed=discord.Embed(title=f"Cancelled {name}'s Application'"))
+		await ctx.send(embed=discord.Embed(title=f"Cancelled {name}'s Application'"))
 	
 			
 def setup(bot):
