b4d6af149d81f62688c41569fcee91dbd3a6da11
Rukchad Wongprayoon <mooping3roblox@gmail.com>
2022-03-14 03:35:18 +0000

    Initial commit

--- src/cogs/guild_finder.py
+++ src/cogs/guild_finder.py
@@ -6,24 +6,28 @@ import aiofiles
 class Guild_Finder(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.guild_finder_task.start()
-    
-    @tasks.loop(seconds=5)
+        
     async def guild_finder_task(self):
-        await self.bot.wait_until_ready()
-        guilds = self.bot.guilds
-        async with aiofiles.open("src/cogs/db/db.json") as fp:
-            data = await fp.read()
-            data = json.loads(data)
-        
-        for guild in guilds:
-            if str(guild.id) not in data:
-                data[str(guild.id)] = {
-                    "applications": {},
-                }
-        
-        async with aiofiles.open("src/cogs/db/db.json", "w") as fp:
-            await fp.write(json.dumps(data))
+        while True:
+            await self.bot.wait_until_ready()
+            guilds = self.bot.guilds
+            async with aiofiles.open("src/cogs/db/db.json") as fp:
+                data = await fp.read()
+                data = json.loads(data)
+            
+            for guild in guilds:
+                if str(guild.id) not in data:
+                    data[str(guild.id)] = {
+                        "applications": {},
+                    }
+            
+            async with aiofiles.open("src/cogs/db/db.json", "w") as fp:
+                await fp.write(json.dumps(data))
+
+        await asyncio.sleep(5)
+
+    async def setup_hook(self):
+        self.loop.create_task(self.guild_finder_task())
 
 def setup(bot):
     bot.add_cog(Guild_Finder(bot))
